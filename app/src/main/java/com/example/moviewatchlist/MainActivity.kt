33ca2a426397fe801e741c88609c58629f124b90
mcicishvilii <83747561+mcicishvilii@users.noreply.github.com>
2022-04-18 00:21:01 +0400

    ყველაფერი გავაკეთე გარდა იმისა რო როცა დავაჭერ თასქს არ ქრება. მწვანდება მაგრამ არ ქვრება

--- app/src/main/java/com/example/moviewatchlist/MainActivity.kt
+++ app/src/main/java/com/example/moviewatchlist/MainActivity.kt
@@ -1,62 +1,59 @@
 package com.example.moviewatchlist
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.moviewatchlist.databinding.ActivityMainBinding
+import com.example.moviewatchlist.databinding.LayoutMovieItemBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.RVMovies.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.RVTodoList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-        binding.RVMovies.adapter = MoviesAdapter(
+
+
+
+        binding.RVTodoList.adapter = MoviesAdapter(
             mutableListOf(
-                Movie(
-                    "Total recall",
-                    "https://i.scdn.co/image/ab67616d0000b273f528e423cc48728f36f7c353",
-                ),
-                Movie(
-                    "Shawshank redemption",
-                    "https://upload.wikimedia.org/wikipedia/ka/8/81/ShawshankRedemptionMoviePoster.jpg",
-                ),
-                Movie(
-                    "Leon the professional",
-                    "https://upload.wikimedia.org/wikipedia/ka/8/81/ShawshankRedemptionMoviePoster.jpg",
-                ),
-                Movie(
-                    "Inception",
-                    "https://upload.wikimedia.org/wikipedia/ka/8/81/ShawshankRedemptionMoviePoster.jpg",
-                ),
-                Movie("Usual suspects", "https://flxt.tmsimg.com/assets/p16422_p_v8_ag.jpg"),
-                Movie("The Shining", "https://movieposters2.com/images/1511858-b.jpg"),
-                Movie("American beauty", "https://flxt.tmsimg.com/assets/p23514_p_v12_ah.jpg"),
-                Movie(
-                    "Dark knight",
-                    "https://upload.wikimedia.org/wikipedia/ka/8/81/ShawshankRedemptionMoviePoster.jpg",
-                ),
-                Movie(
-                    "Dark knight",
-                    "https://upload.wikimedia.org/wikipedia/ka/8/81/ShawshankRedemptionMoviePoster.jpg",
-                ),
-            )).apply {
-            setOnItemClickListener {movie: Movie, i: Int ->
-                movieList.add(i,
-                    Movie("The Shining", "https://movieposters2.com/images/1511858-b.jpg")
+                Todo(
+                    ""
                 )
-                notifyItemInserted(0)
-            }
-        }
+            )
+        ).apply {
 
+            binding.BTNAddbutton.setOnClickListener {
+
+
+                todoListList.add(Todo(binding.ETEnterTask.text.toString()))
+                notifyDataSetChanged()
+
+                Toast.makeText(this@MainActivity,"${binding.ETEnterTask.text.toString()} added",Toast.LENGTH_SHORT).show()
+            }
+
+            setOnItemCLickListener { task: Todo, i: Int ->
+                todoListList.remove(task)
+                notifyItemChanged(0)
+                notifyItemRangeChanged(0,5)
+
+                Toast.makeText(this@MainActivity,"${binding.ETEnterTask.text.toString()} completed",Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
     }
 }
+
+
+
+
+
---
+++ app/src/main/java/com/example/moviewatchlist/TodolistAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.moviewatchlist
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.moviewatchlist.databinding.LayoutMovieItemBinding
+
+class MoviesAdapter( var todoListList: MutableList<Todo>) : RecyclerView.Adapter<TodolistViewHolder>() {
+
+
+    private lateinit var itemClickListener: (Todo,Int) -> Unit
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodolistViewHolder {
+        val binding =
+            LayoutMovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodolistViewHolder(binding)
+    }
+
+
+
+    fun setOnItemCLickListener(clickListener: (Todo,Int) -> Unit) {
+        itemClickListener = clickListener
+    }
+
+
+    override fun onBindViewHolder(holder: TodolistViewHolder, position: Int) {
+        val task = todoListList[position]
+        holder.binding.TWTaskName.text = task.taskName
+
+        holder.binding.TWTaskName.setOnClickListener {
+            itemClickListener.invoke(task,position)
+            holder.binding.indicator.setBackgroundColor(Color.GREEN)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todoListList.size
+    }
+}
+
+data class Todo(
+    var taskName: String
+)
+
+class TodolistViewHolder(val binding: LayoutMovieItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+}
