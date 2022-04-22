f871d5712fe92ff0ab3e98a65994b38384d1d323
mcicishvilii <83747561+mcicishvilii@users.noreply.github.com>
2022-04-22 10:47:23 +0400

    წავშალე mutableListOf (Todo) და გადავეცი ცარიელი ლისტი და პრიველი აითემი სუ რო ეხატებოდა წაიშალა

--- app/src/main/java/com/example/moviewatchlist/MainActivity.kt
+++ app/src/main/java/com/example/moviewatchlist/MainActivity.kt
@@ -15,7 +15,7 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var binding2: LayoutMovieItemBinding //ეს ცვლადი აქ არ გჭირდება
+
 
 
     @SuppressLint("ResourceType")
@@ -26,48 +26,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding2 = LayoutMovieItemBinding.inflate(layoutInflater)
-//        setContentView(binding2.root)
-//        binding2 = LayoutMovieItemBinding.inflate(layoutInflater)
-//        setContentView(binding2.root)
-
-//        var redcolor: View? = findViewById(R.color.red)
-//        var indicator:ImageView = findViewById(R.id.indicator)
-//        var item:View? = findViewById(R.id.TW_taskName)
-
-
-
-
         binding.RVTodoList.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-
-//        binding2.indicator.setOnClickListener {
-//
-//            if (binding2.indicator.colorFilter == redcolor) {
-//                binding2.indicator.isClickable = true
-//                Toast.makeText(
-//                    this@MainActivity,
-//                    "${binding.ETEnterTask.text.toString()} mate",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//
-//            } else {
-//                Toast.makeText(
-//                    this@MainActivity,
-//                    "${binding.ETEnterTask.text.toString()} completed",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//            }
-//        }
         // შეგიძლია ადაპტერს გადასცე ცარიელი სია პირდაპირ  mutableListOf<Todo>()
 
             binding.RVTodoList.adapter = MoviesAdapter(
-                mutableListOf(
-                    Todo(
-                        ""
-                    )
-                )
+                mutableListOf<Todo>()
             ).apply {
 
                 binding.BTNAddbutton.setOnClickListener {
@@ -75,11 +40,6 @@ class MainActivity : AppCompatActivity() {
                     if (binding.ETEnterTask.text.isNotEmpty()) {
                         todoListList.add(Todo(binding.ETEnterTask.text.toString()))
                         notifyDataSetChanged()
-                        Toast.makeText(
-                            this@MainActivity,
-                            "${binding.ETEnterTask.text.toString()} added",
-                            Toast.LENGTH_SHORT
-                        ).show()
                         binding.ETEnterTask.text.clear()
 
 
@@ -88,25 +48,6 @@ class MainActivity : AppCompatActivity() {
                         binding.ETEnterTask.setHintTextColor(Color.RED)
                         binding.TWEnterTodo.setTextColor(Color.RED)
                     }
-
-//                item?.setOnClickListener {
-//                    if(indicator.colorFilter == redcolor){
-//                        Toast.makeText(
-//                            this@MainActivity,
-//                            "witelia",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }else{
-//                        Toast.makeText(
-//                            this@MainActivity,
-//                            "araa",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//                }
-
-
-
 
                     setOnItemCLickListener { task: Todo, i: Int ->
                         todoListList.remove(task)
